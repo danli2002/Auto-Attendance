3a0f52a13dbeeb06d2499bfcd18f536be63ba3b7
Daniel <dan.li2010@hotmail.com>
2020-09-16 10:51:44 -0400

    Add files via upload
    updated for 2020-21

--- formFiller.py
+++ formFiller.py
@@ -1,8 +1,6 @@
 '''
-
 This is an early version of the PHS Auto Attendance Script
 Just specify your info in the file config.txt
-
 Entry IDs are HARDCODED. They will become more flexible in the future by smartly searching for more data entry fields.
 '''
 
@@ -12,7 +10,7 @@ import os
 
 # Parses the URL from the given command line arguments. Not very useful at the moment, but when the script becomes more flexible for other forms, it will.
 
-url = "https://docs.google.com/forms/d/e/1FAIpQLSfpe6jz-GIykefd3TOQ0pax7-PxsJWRaCrivZFOnkijA43seA/formResponse"
+url = "https://docs.google.com/forms/d/e/1FAIpQLSckymPmvHPzYxhNx6hgD-dR-WETt7eD3S4JzD4rm7TafINZjg/formResponse"
 
 user_data = []
 
@@ -22,23 +20,24 @@ with open(('config.txt'), 'r') as config:
 
 user_data = [data.strip() for data in user_data]  # ensure no leading/trailing spaces in data
 
-if len(user_data) != 3:  # data doesn't have 3 lines
+if len(user_data) != 4:  # data doesn't have 3 lines
     print('There was an error reading from the config file. Please ensure that the file contains your first name, last name, and grade number on 3 separate lines.')
 else:
     grade_levels = {
-        '9': 'Freshman',
-        '10': 'Sophomore',
-        '11': 'Junior',
-        '12': 'Senior',
+        '9': 'Grade 9',
+        '10': 'Grade 10',
+        '11': 'Grade 11',
+        '12': 'Grade 12',
     }
 
     # Dictionary of all the entry values
     # Test entries: entry.2005620554 , entry.1624489572 , entry.839337160
     # Real entries: entry.37835231 , entry.477309531 , entry.793669177
     submissions = {
-        'entry.37835231': user_data[0],
-        'entry.477309531': user_data[1],
-        'entry.793669177': grade_levels[user_data[2]],
+        'emailAddress': user_data[0],
+        'entry.1657521344': user_data[1],
+        'entry.502163738': user_data[2],
+        'entry.1429504230': grade_levels[user_data[3]],
     }
 
     # Initiates a POST to the server
